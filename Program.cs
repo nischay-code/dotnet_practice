15dab3a41ad856ac350533fffa2d86d92fda7235
nischay-code <nischay.patel.code@gmail.com>
2023-07-15 01:04:44 +0530

    Variables and Constants

--- Program.cs
+++ Program.cs
@@ -1,13 +1,28 @@
-﻿
+﻿//Hello World
 // Console.WriteLine("Hello, World!");
+
+
+//Input
 // string str = Console.ReadLine();
 // Console.WriteLine(str);
 
-int a = 3;
-int b = 5;
-int c = a + b;
-int d = a - b;
-Console.Write("Sum : ");
-Console.WriteLine(c);
-// Console.WriteLine("The Sum is {0}", c);
-Console.WriteLine("The Difference is {0}", d);
+
+//Identifiers and Keywords
+// int a = 3;
+// int b = 5;
+// int c = a + b;
+// int d = a - b;
+// Console.Write("Sum : ");
+// Console.WriteLine(c);
+// // Console.WriteLine("The Sum is {0}", c);
+// Console.WriteLine("The Difference is {0}", d);
+
+
+//Variables and Constants
+const double pi = 3.14;  //constant declaration
+double r;
+r = 10;
+double area = pi * r * r;
+double circumference = 2 * pi * r;
+Console.WriteLine("The Area of Circle is {0}", area);
+Console.WriteLine("The Circumference of Circle is {0}", circumference);
