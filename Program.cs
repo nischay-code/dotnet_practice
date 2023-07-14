7550316fd113cb1af8a93f3ba4763b595f4ae652
nischay-code <nischay.patel.code@gmail.com>
2023-07-15 00:57:21 +0530

    Identifiers and Keywords

--- Program.cs
+++ Program.cs
@@ -1,4 +1,13 @@
 ﻿
-Console.WriteLine("Hello, World!");
-string str = Console.ReadLine();
-Console.WriteLine(str);
+// Console.WriteLine("Hello, World!");
+// string str = Console.ReadLine();
+// Console.WriteLine(str);
+
+int a = 3;
+int b = 5;
+int c = a + b;
+int d = a - b;
+Console.Write("Sum : ");
+Console.WriteLine(c);
+// Console.WriteLine("The Sum is {0}", c);
+Console.WriteLine("The Difference is {0}", d);
