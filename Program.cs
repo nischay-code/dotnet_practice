511d246352babf116d842dcfd443ef1c0372c41e
nischay-code <nischay.patel.code@gmail.com>
2023-07-15 15:12:47 +0530

    Controlflow Statements

--- Program.cs
+++ Program.cs
@@ -57,25 +57,83 @@
 
 
 // Expressions and Operators
-int result;
-int x = 11, y = 5;
-int a = 20, b;
-int result1;
-int r = 5, s = 9, t = 3;
-// Division 
-result = (x / y);
-Console.WriteLine("Division Operator : " + result);
-//Modulo
-result = (x % y);
-Console.WriteLine("Modulo Operator : " + result);
-//Post-increment
-b = a++;
-Console.WriteLine("a is {0} and Post-increment is {1}", a, b);
-//Bitwise AND Operator
-result1 = (r & s);
-Console.WriteLine("Bitwise AND : " + result1);
-t += 10;
-Console.WriteLine("Add Assignment Operator : " + t);
+// int result;
+// int x = 11, y = 5;
+// int a = 20, b;
+// int result1;
+// int r = 5, s = 9, t = 3;
+// // Division 
+// result = (x / y);
+// Console.WriteLine("Division Operator : " + result);
+// //Modulo
+// result = (x % y);
+// Console.WriteLine("Modulo Operator : " + result);
+// //Post-increment
+// b = a++;
+// Console.WriteLine("a is {0} and Post-increment is {1}", a, b);
+// //Bitwise AND Operator
+// result1 = (r & s);
+// Console.WriteLine("Bitwise AND : " + result1);
+// t += 10;
+// Console.WriteLine("Add Assignment Operator : " + t);
+
+
+// Controlflow Statements - Decision making Statements
+//nested if-else condition 
+// int num1 = 3, num2 = 5, num3 = 2;
+// if (num1 > num2)
+// {
+//     if (num1 > num3)
+//     {
+//         Console.WriteLine("{0} is the Largest", num1);
+//     }
+//     else
+//     {
+//         Console.WriteLine("{0} is the Largest", num3);
+//     }
+// }
+// else
+// {
+//     if (num2 > num3)
+//     {
+//         Console.WriteLine("{0} is the Largest", num2);
+//     }
+//     else
+//     {
+//         Console.WriteLine("{0} is the Largest", num3);
+//     }
+
+// }
+//switch-case
+// char ch;
+// Console.WriteLine("Enter the alphabet");
+// ch = Convert.ToChar(Console.ReadLine());
+// switch (ch)
+// {
+//     case 'a':
+//         Console.WriteLine("It is a vowel");
+//         break;
+//     case 'e':
+//         Console.WriteLine("It is a vowel");
+//         break;
+//     case 'i':
+//         Console.WriteLine("It is a vowel");
+//         break;
+//     case 'o':
+//         Console.WriteLine("It is a vowel");
+//         break;
+//     case 'u':
+//         Console.WriteLine("It is a vowel");
+//         break;
+//     default:
+//         Console.WriteLine("It is not a vowel");
+//         break;
+// }
+//ternary operator
+int num = 4;
+bool checkNum;
+checkNum = (num % 2 == 0);
+Console.WriteLine(checkNum);
 
 
 
